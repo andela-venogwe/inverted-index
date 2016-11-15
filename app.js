78a1a7cb89909cc42af27a4641328d3c437e8aff
andela-venogwe <victor.enogwe@gmail.com>
2016-11-15 01:17:38 +0100

    setup express app and sass middleware

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
+"use strict";
+
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const sass    = require('node-sass');
-const path    = require('path');
+const sass = require('node-sass-middleware');
 const index = require('./routes/index');
-//var users = require('./routes/users');
 
 const app = express();
 
@@ -15,22 +14,24 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-/** override the static method above using middleware sass
+
+/** app.use(express.static(path.join(__dirname, 'public')));
+ * override the static method above using middleware sass
  * adding the sass middleware The reason is that 
  * we first want sass to compile any sass files that 
  * has changed, only then serve them (which is done by express.static).
  */
+
 app.use(
-  sass.middleware({
+  sass({
     src: __dirname + '/sass', 
-    dest: __dirname + '/public',
+    dest: __dirname + '/public/stylesheets',
+    outputStyle: 'compressed',
     prefix:  '/stylesheets',
     debug: true,       
   })
@@ -39,17 +40,16 @@ app.use(
 app.use(express.static( path.join( __dirname, 'public' ) ) );
 
 app.use('/', index);
-//app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
