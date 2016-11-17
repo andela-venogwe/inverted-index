f4b5f0edfa0c5b3e5c1e3b76d28a3c20653e8b90
andela-venogwe <victor.enogwe@andela.com>
2016-11-17 09:37:10 +0100

    chore(gulp) add gulp modules
    add gulp modules for jasmine, nodemon, jshint, coveralls to gulp process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,68 @@
 const gulp        = require('gulp');
-const sass        = require('gulp-ruby-sass');
 const browserSync = require('browser-sync').create();
+const nodemon = require('gulp-nodemon');
+const jshint = require('gulp-jshint');
+const jasmineBrowser = require('gulp-jasmine-browser');
 const coveralls = require('gulp-coveralls');
-// Static Server + watching scss/jade files
-gulp.task('serve', ['sass', 'js'], () => {
-  'use srict';
-  browserSync.init({
-    server: "./app"
-  });
 
-  gulp.watch("app/sass/*.scss", ['sass']);
-  gulp.watch("app/views/*.jade").on('change', browserSync.reload);
+// watch for css jade and js file changes and reload browser
+gulp.task('watch', () => {
+  'use strict';
+  gulp.watch(['src/*.js', 'src/sass/*.scss', 'public/javascripts/*.js'], browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', () => {
-  return gulp.src("app/scss/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("app/css"))
-    .pipe(browserSync.stream());
+//gulp jshint code testing
+gulp.task('lint', () => {
+  'use strict';
+  return gulp.src(['*.js', 'src/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
-// process JS files and return the stream.
-gulp.task('js', () => {
-  return gulp.src('src/*js')
-    .pipe(browserify())
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+//re-run jasmine tests on file change
+gulp.task('jasmine', ['lint'], () => {
+  'use strict';
+  return gulp.src('jasmine/spec/*.js')
+    .pipe(jasmineBrowser.specRunner())
+    .pipe(jasmineBrowser.server({port: 8888}));
 });
-
 
 // gulp coverall testing
-gulp.task('coveralls', function() {
+gulp.task('coveralls', ['jasmine'], () => {
+  'use strict';
   return gulp.src('./jasmine/spec/lcov.info')
     .pipe(coveralls());
 });
 
-gulp.task('default', ['serve']);
+// run the default task
+gulp.task('default', ['watch', 'browser-sync']);
+
+// run browsersync after nodemon runs
+gulp.task('browser-sync', function() {
+  browserSync.init(null, {
+    proxy: "http://localhost:3000",
+    browser: "google chrome",
+    port: 5000,
+    ui: {
+      port: 5001
+    },
+    reloadOnRestart: true
+  });
+});
+
+// run the nodemon server reload
+gulp.task('nodemon', function (cb) {
+  const started = false;
+  return nodemon({
+    script: 'app.js',
+    ext: 'js', 
+    env: { 'NODE_ENV': 'development' }
+  }).on('start', function () {
+    // to avoid nodemon being started multiple times
+    // thanks @matthisk
+    if (!started) {
+      cb();
+      started = true; 
+    } 
+  });
+});
