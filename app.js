57523015b4873d3b9c8d660a11f824c203520cd0
Enogwe Victor <victor.enogwe@andela.com>
2016-11-22 05:47:20 +0100

    Revert "chore(Integrate Services) - features(Build UI)"

--- app.js
+++ app.js
@@ -2,52 +2,54 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const sass = require('node-sass-middleware');
+const sass    = require('node-sass');
+const path    = require('path');
 const index = require('./routes/index');
+//var users = require('./routes/users');
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, 'src/public', 'favicon.ico')));
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-/** app.use(express.static(path.join(__dirname, 'public')));
- * override the static method above using middleware sass
- * adding the sass middleware The reason is that
- * we first want sass to compile any sass files that
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+/** override the static method above using middleware sass
+ * adding the sass middleware The reason is that 
+ * we first want sass to compile any sass files that 
  * has changed, only then serve them (which is done by express.static).
  */
 app.use(
-  sass({
-    src: path.join(__dirname, '/src/sass'),
-    dest: path.join(__dirname, '/src/public/css'),
-    outputStyle: 'compressed',
-    prefix: '/css',
-    debug: true,
+  sass.middleware({
+    src: __dirname + '/sass', 
+    dest: __dirname + '/public',
+    prefix:  '/stylesheets',
+    debug: true,       
   })
 );
-
 // The static middleware must come after the sass middleware
-app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(express.static( path.join( __dirname, 'public' ) ) );
 
 app.use('/', index);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use((err, req, res) => {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- src/jasmine/spec/inverted-index-test_spec.js
+++
@@ -1,9 +0,0 @@
-describe("Read book data", () => {
-  it("should read the JSON file and assert that it is not empty", function() {
-    expect(['1', 2].length > 1).toBe(true);
-  });
-
-  it("should read", function() {
-    expect(['1'].length === 1).toBe(true);
-  });
-}); 
