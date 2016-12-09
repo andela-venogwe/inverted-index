eb408f31920fd2ae28c7812946d708861a6e1a8d
andela-venogwe <victor.enogwe@andela.com>
2016-12-09 10:19:59 +0100

    still refactoring the jasmine async test to work with karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
-// Generated on Fri Dec 02 2016 09:50:30 GMT+0100 (WAT)
+// Generated on Fri Dec 09 2016 07:10:21 GMT+0100 (WAT)
 
-module.exports = (config) => {
+module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -15,73 +15,68 @@ module.exports = (config) => {
 
     // list of files / patterns to load in the browser
     files: [
-      './src/public/js/Inverted-Index-Utility.js',
-      './src/public/js/Inverted-index.js',
-      './src/jasmine/spec/inverted-index-test.js',
+      'src/public/js/*.js',
+      'src/jasmine/spec/*.js'
     ],
+
 
     // list of files to exclude
     exclude: [
-      './coverage/',
+      'src/public/js/app.js'
     ],
 
 
     // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      './src/public/js/Inverted-Index-Utility.js': 'coverage',
-      './src/public/js/Inverted-index.js': 'coverage',
-      './src/jasmine/spec/inverted-index-test.js': 'coverage',
     },
 
+    // // Which plugins to enable
+    // plugins: [
+    //   'karma-chrome-launcher',
+    //   'karma-coverage',
+    //   'karma-coveralls',
+    //   'karma-jasmine',
+    //   'karma-spec-reporter',
+    //   'karma-verbose-reporter',
+    // ],
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['spec', 'coverage', 'coveralls', 'verbose'],
 
-    coverageReporter: {
-      type: 'lcov',
-      dir: 'coverage/'
-    },
-
     // web server port
     port: 9876,
 
 
+    proxies: {
+      '/src/public/uploads/': '/src/public/uploads/',
+    },
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-    proxies: {
-      '/src/public/uploads/': 'http://localhost:9876/src/public/uploads/'
-    },
 
     // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
-    // config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_ERROR,
 
 
-    // enable / disable watching file on file change
+    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
 
-    // Others
-    customLaunchers: {
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
-
     // start these browsers
-    browsers: process.env.TRAVIS ? ['Chrome_travis_ci'] : ['Chrome'],
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: false,
 
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
-  });
-};
+  })
+}
